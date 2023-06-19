8f35c7a230a4092548ffdab8cb0f252fd8bb7c7e
Kanstrup <krjoh21@student.sdu.dk>
2023-06-19 15:25:10 +0200

    Master THeroem

---
+++ AD/Scripts/master_theorem.py
@@ -0,0 +1,16 @@
+import math
+
+def master_theorem(a, b, c):
+    # compute log_b(a)
+    log_b_a = math.log(a, b)
+    if c < log_b_a:
+        return "T(n) = Θ(n^" + str(log_b_a) + ")"
+    elif c == log_b_a:
+        return "T(n) = Θ(n^" + str(c) + " * log(n))"
+    elif c > log_b_a:
+        return "T(n) = Θ(n^" + str(c) + ")"
+    else:
+        return "Input Error!"
+
+# example usage
+print(master_theorem(2, 3, 1))
